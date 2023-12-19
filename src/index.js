ff00ce59dd26073ed7aef8700385ed45d3870c43
Cecilia <ccchimento@gmail.com>
2023-12-19 12:35:12 -0700

    edits after code walkthrough

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 // write your code here
 /* advanced:
 1)
-2) Update the rating and comment for a ramen by submitting a form.
-Changes should be reflected on the frontend. No need to persist.
-You can add this HTML to the index.html file to create the edit form:
 3) Delete a ramen (you can add a "delete" button if you'd like,
 or use an existing element to handle the delete action).
 The ramen should be removed from the ramen-menu div, and 
@@ -11,10 +8,12 @@ should not be displayed in the ramen-detail div. No need to persist.*/
 
 fetch('http://localhost:3000/ramens')
     .then(resp => resp.json())
-    .then(data => {addRamen(data)
+    .then(data => {
+        addRamen(data)
         addFirstRamen(data)})
 
 const ramenMenuDiv = document.querySelector('#ramen-menu')
+const ramenDetail = document.querySelector('#ramen-detail')
 const detailImg = document.querySelector('.detail-image')
 const detailName = document.querySelector('.name')
 const detailRestaurant = document.querySelector('.restaurant')
@@ -22,6 +21,10 @@ const detailRating = document.querySelector('#rating-display')
 const detailComment = document.querySelector('#comment-display')
 const ramenForm = document.querySelector('#new-ramen')
 const editForm = document.querySelector('#edit-ramen')
+const deleteBtn = document.createElement('button')
+deleteBtn.textContent = "DELETE THIS RAMEN"
+ramenDetail.appendChild(deleteBtn)
+
 
 function addFirstRamen(ramenArr){
     let ramenObj = ramenArr[0];
@@ -34,8 +37,9 @@ function addFirstRamen(ramenArr){
 
 function addRamen(ramenArr){
     ramenArr.forEach(ramen => {
-        let ramenImg = document.createElement('img');
+        const ramenImg = document.createElement('img');
         ramenImg.src = ramen.image;
+        ramenImg.id = ramen.id
         ramenMenuDiv.appendChild(ramenImg);
         ramenImg.addEventListener('click', () => {
             detailImg.src = ramen.image;
@@ -47,36 +51,32 @@ function addRamen(ramenArr){
     })
 }
 
-ramenForm.addEventListener('submit', function(event) {
+ramenForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    const newName = event.target.name.value;
-    const newRestaurent = event.target.restaurant.value;
-    const newImg = event.target.image.value;
-    const newRating = event.target.rating.value;
-    const newComment = document.querySelector('#new-comment').value
     const newRamenObj = {
-        name : newName,
-        restaurant : newRestaurent,
-        image : newImg,
-        rating : newRating,
-        comment : newComment
+        name : event.target.name.value,
+        restaurant : event.target.restaurant.value,
+        image : event.target.image.value,
+        rating : event.target.rating.value,
+        comment : event.target['new-comment'].value
     }
-    let newRamenImg = document.createElement('img');
-    newRamenImg.src = newRamenObj.image;
-    ramenMenuDiv.appendChild(newRamenImg);
-    newRamenImg.addEventListener('click', () => {
-        detailImg.src = newRamenObj.image;
-        detailName.textContent = newRamenObj.name;
-        detailRestaurant.textContent = newRamenObj.restaurant;
-        detailRating.textContent = newRamenObj.rating;
-        detailComment.textContent = newRamenObj.comment;
-    })
+    addRamen([newRamenObj]); //change newRamenObj to array to avoid coding the following:
+    // let newRamenImg = document.createElement('img');
+    // newRamenImg.src = newRamenObj.image;
+    // ramenMenuDiv.appendChild(newRamenImg);
+    // newRamenImg.addEventListener('click', () => {
+    //     detailImg.src = newRamenObj.image;
+    //     detailName.textContent = newRamenObj.name;
+    //     detailRestaurant.textContent = newRamenObj.restaurant;
+    //     detailRating.textContent = newRamenObj.rating;
+    //     detailComment.textContent = newRamenObj.comment;
+    // })
 })
 
 editForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const editRating = e.target.rating.value;
-    const editComment = document.querySelector("#edit-comment").value;
+    const editComment = e.target['edit-comment'].value;
     detailRating.textContent = editRating;
     detailComment.textContent = editComment;
 })
